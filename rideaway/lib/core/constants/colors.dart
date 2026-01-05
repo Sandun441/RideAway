75de248053ca20a8e7bb84a6cb4314cee17faa25
Sandun441 <117435121+Sandun441@users.noreply.github.com>
2026-01-05 20:46:37 +0530

    updated

--- rideaway/lib/core/constants/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primary = Color(0xFF1565C0);
-  static const accent = Color(0xFFFF5252);
-  static const background = Color(0xFFF5F7FA);
-  static const success = Color(0xFF2E7D32);
-}
